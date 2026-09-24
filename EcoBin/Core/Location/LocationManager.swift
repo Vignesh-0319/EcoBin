69c9c33cc20fb1dcf0a91308feede988fb82236b
Vignesh <vignesh0319.becse24@chitkara.edu.in>
2026-09-24 10:46:28 +0530

    new update in code

--- EcoBin/Core/Location/LocationManager.swift
+++ EcoBin/Core/Location/LocationManager.swift
@@ -5,3 +5,71 @@
 //  Created by SUPER CHARGE on 24/09/26.
 //
 
+import Foundation
+import CoreLocation
+
+@Observable
+final class LocationManager: NSObject, CLLocationManagerDelegate {
+    static let shared = LocationManager()
+    
+    private let manager = CLLocationManager()
+    var userLocation: CLLocationCoordinate2D?
+    var authorizationStatus: CLAuthorizationStatus = .notDetermined
+    var isNearTargetBin: Bool = false
+    
+    private var targetCheckInRegion: CLCircularRegion?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.distanceFilter = 10
+    }
+
+    func requestPermission() {
+        manager.requestWhenInUseAuthorization()
+    }
+
+    func startTracking() {
+        manager.startUpdatingLocation()
+    }
+
+    func stopTracking() {
+        manager.stopUpdatingLocation()
+    }
+
+    func setCheckInGeofence(for coordinate: CLLocationCoordinate2D, radiusInMeters: CLLocationDistance = 25.0) {
+        let region = CLCircularRegion(
+            center: coordinate,
+            radius: radiusInMeters,
+            identifier: "BinCheckInZone"
+        )
+        region.notifyOnEntry = true
+        region.notifyOnExit = true
+        self.targetCheckInRegion = region
+        
+        // Immediate check against current location
+        if let current = userLocation {
+            let userLoc = CLLocation(latitude: current.latitude, longitude: current.longitude)
+            let targetLoc = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+            isNearTargetBin = userLoc.distance(from: targetLoc) <= radiusInMeters
+        }
+    }
+
+    // MARK: - CLLocationManagerDelegate
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+        if authorizationStatus == .authorizedWhenInUse || authorizationStatus == .authorizedAlways {
+            startTracking()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let latest = locations.last else { return }
+        userLocation = latest.coordinate
+
+        if let region = targetCheckInRegion {
+            isNearTargetBin = region.contains(latest.coordinate)
+        }
+    }
+}
--- EcoBin/Models/Domains/WasteBin.swift
+++ EcoBin/Models/Domains/WasteBin.swift
@@ -5,3 +5,61 @@
 //  Created by SUPER CHARGE on 24/09/26.
 //
 
+import Foundation
+import CoreLocation
+import SwiftUI
+
+enum BinType: String, Codable, CaseIterable {
+    case general = "General"
+    case recyclable = "Recyclable"
+    case organic = "Organic"
+    case hazardous = "Hazardous"
+    
+    var iconName: String {
+        switch self {
+        case .general: return "trash.fill"
+        case .recyclable: return "arrow.3.trianglepath"
+        case .organic: return "leaf.fill"
+        case .hazardous: return "exclamationmark.shield.fill"
+        }
+    }
+}
+
+enum FillLevelTier: String, Codable {
+    case optimal
+    case moderate
+    case high
+    case critical
+    
+    var color: Color {
+        switch self {
+        case .optimal: return .green
+        case .moderate: return .yellow
+        case .high: return .orange
+        case .critical: return .red
+        }
+    }
+}
+
+struct WasteBin: Identifiable, Codable, Hashable {
+    let id: UUID
+    let binCode: String
+    var coordinate: CLLocationCoordinate2D
+    var type: BinType
+    var fillPercentage: Int
+    var batteryLevel: Int
+    var lastEmptied: Date
+    var isDamaged: Bool
+    
+    var tier: FillLevelTier {
+        switch fillPercentage {
+        case 0..<50: return .optimal
+        case 50..<75: return .moderate
+        case 75..<90: return .high
+        default: return .critical
+        }
+    }
+
+    static func == (lhs: WasteBin, rhs: WasteBin) -> Bool { lhs.id == rhs.id }
+    func hash(into hasher: inout Hasher) { hasher.combine(id) }
+}
