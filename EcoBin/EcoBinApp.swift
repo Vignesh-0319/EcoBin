1f86cc90acda29bd6e3c5dfbcf09e24cce4ddd1e
Vignesh <vignesh0319.becse24@chitkara.edu.in>
2026-09-24 09:56:27 +0530

    Initial Commit

---
+++ EcoBin/EcoBinApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoBinApp.swift
+//  EcoBin
+//
+//  Created by SUPER CHARGE on 24/09/26.
+//
+
+import SwiftUI
+
+@main
+struct EcoBinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
